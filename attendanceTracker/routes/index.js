ea3cdb7232f751b4ddf3c1de82bea15103da8f38
Sindre <sndrem@live.no>
2016-09-05 22:12:20 +0200

    Startet på login og register-sidene

--- attendanceTracker/routes/index.js
+++ attendanceTracker/routes/index.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Tregas' });
 });
 
+/* Login route */
+router.get("/login", function(req, res, next) {
+	res.render("login", {title: "Log in"});
+});
+
+/* Register route */
+router.get("/register", function(req, res, next) {
+	res.render("register", {title: "Create New User"});
+});
+
 module.exports = router;
