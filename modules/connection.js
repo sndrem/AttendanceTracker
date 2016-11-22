0fa2f760442cb627d11d867a02bc1b219fde18b8
Sindre Moldeklev <sndrem@live.no>
2016-11-22 12:58:49 +0100

    Beklager spam. Legger med nedpå ang. å pushe applikasjonen live. Får fikse det en annen gang

--- modules/connection.js
+++ modules/connection.js
@@ -3,9 +3,18 @@ var connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PWD,
+    port: process.env.DB_PORT,
     database: process.env.DB_DATABASE,
     multipleStatements: true
 });
+
+connection.connect(function(err) {
+    if(err) {
+        console.log("Error connecting to database. Please check credentials");
+    } else {
+        console.log("Connected to database. Have a nice day");
+    }
+})
 
 console.log("Creating connection and connecting to database");
 console.log(connection);
