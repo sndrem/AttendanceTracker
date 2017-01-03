4092a9db9aac4b9140b5372c08bae3be299adf41
Sindre Moldeklev <sndrem@live.no>
2017-01-03 14:48:58 +0100

    Added code for handling server disconnect

--- modules/connection.js
+++ modules/connection.js
@@ -1,22 +1,37 @@
 var mysql = require('mysql');
-console.log(process.env);
-var connection = mysql.createConnection({
+var dbConfig = {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PWD,
     port: process.env.DB_PORT,
     database: process.env.DB_DATABASE,
     multipleStatements: true
-});
+};
 
-console.log('Hello from the connection file');
-connection.connect(function(err) {
-    if(err) {
-        console.log(err);
-        console.log("Error connecting to database. Please check credentials");
-    } else {
-        console.log("Connected to database. Have a nice day");
-    }
-})
+var connection;
+
+function handleDisconnect() {
+    connection = mysql.createConnection(dbConfig);
+
+    connection.connect(function(err) {
+        if(err) {
+            console.log("Error connecting to database.", err);
+            setTimeout(handleDisconnect, 2000);
+        } else {
+            console.log("Connected to database. Have a nice day");
+        }
+    })
+
+    connection.on('error', function(err) {
+        console.log('db error', err);
+        if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+          handleDisconnect();                         // lost due to either server restart, or a
+        } else {                                      // connnection idle timeout (the wait_timeout
+          throw err;                                  // server variable configures this)
+        }
+      });
+}
+
+handleDisconnect();
 
 module.exports = connection;
