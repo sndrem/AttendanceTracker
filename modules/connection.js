e7b333b22ddc99490d1e89ce57ed588a25b4d986
Sindre Moldeklev <sndrem@live.no>
2017-01-04 10:11:28 +0100

    Added code for utilizing MySQL pooling so that hopefully more than one person can be connected to the database at any given time. The connection limit is now set to 10 connections, since that is the limit of our free ClearDB ignite instance

--- modules/connection.js
+++ modules/connection.js
@@ -1,5 +1,6 @@
 var mysql = require('mysql');
 var dbConfig = {
+    connectionLimit: 10,
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PWD,
@@ -8,30 +9,30 @@ var dbConfig = {
     multipleStatements: true
 };
 
-var connection;
+var pool = mysql.createPool(dbConfig);
 
-function handleDisconnect() {
-    connection = mysql.createConnection(dbConfig);
+// function handleDisconnect() {
+//     connection = mysql.createConnection(dbConfig);
 
-    connection.connect(function(err) {
-        if(err) {
-            console.log("Error connecting to database.", err);
-            setTimeout(handleDisconnect, 2000);
-        } else {
-            console.log("Connected to database. Have a nice day");
-        }
-    })
+//     connection.connect(function(err) {
+//         if(err) {
+//             console.log("Error connecting to database.", err);
+//             setTimeout(handleDisconnect, 2000);
+//         } else {
+//             console.log("Connected to database. Have a nice day");
+//         }
+//     })
 
-    connection.on('error', function(err) {
-        console.log('db error', err);
-        if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-          handleDisconnect();                         // lost due to either server restart, or a
-        } else {                                      // connnection idle timeout (the wait_timeout
-          throw err;                                  // server variable configures this)
-        }
-      });
-}
+//     connection.on('error', function(err) {
+//         console.log('db error', err);
+//         if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+//           handleDisconnect();                         // lost due to either server restart, or a
+//         } else {                                      // connnection idle timeout (the wait_timeout
+//           throw err;                                  // server variable configures this)
+//         }
+//       });
+// }
 
-handleDisconnect();
+// handleDisconnect();
 
-module.exports = connection;
+module.exports = pool;
